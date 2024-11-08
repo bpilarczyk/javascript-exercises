f785037610dfd95ddf6c970219eba6caab5dba8e
BartP <8362400+bpilarczyk@users.noreply.github.com>
2024-11-08 10:28:31 -0500

    Solve Exercise 04

--- 04_removeFromArray/script.js
+++ 04_removeFromArray/script.js
@@ -1,17 +1,19 @@
-const removeFromArray = function(inputArray, ...unwantedElements) {
+function removeFromArray(inputArray, ...unwantedElements) {
 
     let unwantedElementArray = Array.from(unwantedElements);
     let outputArray = inputArray;
-    
+    console.log(unwantedElementArray);
+    // check for duplicates of the unwanted number in the input/output array
+    const duplicates = outputArray.filter((element, index) => outputArray.indexOf(element) !== index);
+
+    console.log(duplicates);
     unwantedElementArray.forEach((item) => {
         while (outputArray.includes(item)) {
+            console.log(`Dupa: ${item}`);
             let indexUnwantedElement = outputArray.indexOf(item);
             outputArray.splice(indexUnwantedElement, 1);
         }
     });
 
     return outputArray;
-};
-
-// Do not edit below this line
-module.exports = removeFromArray;
+}
