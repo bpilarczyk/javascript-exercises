adb3105f2bf3a93800eea6f97a90b16a7426646d
BartP <8362400+bpilarczyk@users.noreply.github.com>
2024-11-08 11:26:27 -0500

    Write 2 temperature conversion functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  let tempInCelsius = parseFloat((((tempInFahrenheit - 32) * 5)/9).toFixed(1));
+  
+  return tempInCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+  let tempInFahrenheit = parseFloat(((9*tempInCelsius)/5 + 32).toFixed(1));
+
+  return tempInFahrenheit;
 };
 
 // Do not edit below this line
